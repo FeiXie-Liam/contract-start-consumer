1f4096ff17ce933bd0a7c72b1ae46bd4f862902f
feixie-liam <xiefei0887@163.com>
2018-08-28 16:50:16 +0800

    [Fei XIE & Yuyang WEI] refactor : format code style

--- src/main/java/com/thoughtworks/contract/consumer/client/ProductClient.java
+++ src/main/java/com/thoughtworks/contract/consumer/client/ProductClient.java
@@ -2,7 +2,6 @@ package com.thoughtworks.contract.consumer.client;
 
 import com.thoughtworks.contract.consumer.entity.Product;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
--- src/main/java/com/thoughtworks/contract/consumer/entity/Product.java
+++ src/main/java/com/thoughtworks/contract/consumer/entity/Product.java
@@ -1,7 +1,6 @@
 package com.thoughtworks.contract.consumer.entity;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
